c090f5af0f6be126b98b022ff85a415b43437ebe
simone-silvestri <silvestri.simone0@gmail.com>
2024-04-28 16:04:31 -0400

    back as it was before

--- experiments/nonhydrostatic_experiment.jl
+++ experiments/nonhydrostatic_experiment.jl
@@ -10,33 +10,32 @@ set_value!(Δh = 200, Δz = 2)
 
 set_value!(; Q, τw, θ, ΔT, Lf, N²)
 
-    # jldopen("experiment$(experiment)_metadata.jld2", parameters = parameters)
+# jldopen("experiment$(experiment)_metadata.jld2", parameters = parameters)
 
-    # Show all the parameters we are using
-    @info "Simulation parameters: " parameters
+# Show all the parameters we are using
+@info "Simulation parameters: " parameters
 
-    stop_time = 30days
-    
-    # Let's start with an nonhydrostatic setup running for 30 days
-    simulation = idealized_setup(architecture; stop_time)
+stop_time = 30days
 
-    # Show the configuration of the simulation
-    @info simulation
+# Let's start with an nonhydrostatic setup running for 30 days
+simulation = idealized_setup(architecture; stop_time)
 
-    # Let's attach some outputs
-    model         = simulation.model
-    output_fields = merge(model.velocities, model.tracers)
+# Show the configuration of the simulation
+@info simulation
 
-    simulation.output_writers[:snapshots] = JLD2OutputWriter(model, output_fields;
-                                                             schedule = TimeInterval(3hours),
-                                                             overwrite_existing = true,
-                                                             filename = "nonhydrostatic_snapshots_$(experiment)")
+# Let's attach some outputs
+model         = simulation.model
+output_fields = merge(model.velocities, model.tracers)
 
-    #####
-    ##### Let's run!!!!
-    #####
+simulation.output_writers[:snapshots] = JLD2OutputWriter(model, output_fields;
+                                                            schedule = TimeInterval(3hours),
+                                                            overwrite_existing = true,
+                                                            filename = "nonhydrostatic_snapshots_$(experiment)")
 
-    run!(simulation)
+#####
+##### Let's run!!!!
+#####
 
-    experiment += 1
-end
+run!(simulation)
+
+experiment += 1
