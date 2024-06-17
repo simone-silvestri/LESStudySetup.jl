5570d882951693403e7021214a59849ad2459b0c
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-06-17 16:00:02 -0400

    better coarsegraining

--- src/Diagnostics/spectral_averaging.jl
+++ src/Diagnostics/spectral_averaging.jl
@@ -4,31 +4,30 @@ using Oceananigans.Units
 using Oceananigans.BoundaryConditions
 using KernelAbstractions: @kernel, @index
 
-function spectral_averaging(u::Field; xcutoff = 8kilometer, ycutoff = 8kilometer)
+function spectral_averaging(u::Field; xcutoff = 20kilometer, ycutoff = 20kilometer)
 
     Δx = u.grid.Δxᶜᵃᵃ
     Δy = u.grid.Δyᵃᶜᵃ
     
-    nx = ceil(Int, xcutoff / Δx)
-    ny = ceil(Int, ycutoff / Δy)
+    nx  = ceil(Int, xcutoff / Δx)
+    ny  = ceil(Int, ycutoff / Δy)
+
+    nx2 = ceil(Int, xcutoff / Δx * π / 2) 
+    ny2 = ceil(Int, xcutoff / Δx * π / 2) 
 
     ū = deepcopy(u)
     d = interior(u)
 
     d̂ = fft(d)
 
-    Nx, Ny, Nz = size(u)
+    @show nx2, ny2, nx, ny
 
-    Nfx  = size(u, 1) ÷ 2 - nx
-    Nfy  = size(u, 2) ÷ 2 - ny
+    Nx, Ny, _ = size(u)
+
     arch = architecture(u)
     grid = u.grid
 
-    xparams = KernelParameters((2nx, Ny, Nz), (Nfx, 0, 0)) 
-    yparams = KernelParameters((Nx, 2ny, Nz), (0, Nfy, 0)) 
-
-    launch!(arch, grid, xparams, _corse_grain!, d̂)
-    launch!(arch, grid, yparams, _corse_grain!, d̂)
+    launch!(arch, grid, :xyz, _corse_grain!, d̂, nx, ny, nx2, ny2, Nx, Ny)
 
     d = ifft(d̂)
     
@@ -38,8 +37,28 @@ function spectral_averaging(u::Field; xcutoff = 8kilometer, ycutoff = 8kilometer
     return ū
 end
 
-@kernel function _corse_grain!(d̂)
+@kernel function _corse_grain!(d̂, nx, ny, nx2, ny2, Nx, Ny)
     i, j, k = @index(Global, NTuple)
-    @inbounds d̂[i, j, k] = 0
-end
+    i′ = ifelse(i > Nx ÷ 2, i - Nx ÷ 2, Nx ÷ 2 - i)
+    j′ = ifelse(j > Ny ÷ 2, j - Ny ÷ 2, Ny ÷ 2 - j)
 
+    # remove the outside frame till nx and ny
+    outside_frame   = (i′ <= nx) | (j′ <= ny)
+    smooth_region_x = (i′ > nx)  & (i′ <= nx2)
+    smooth_region_y = (j′ > ny)  & (j′ <= ny2)
+
+    only_smooth_x = smooth_region_x & !smooth_region_y
+    only_smooth_y = smooth_region_y & !smooth_region_x
+    smooth_both   = smooth_region_x & smooth_region_y
+
+    scaling_x = (i′ - nx) / (nx2 - nx)
+    scaling_y = (j′ - ny) / (ny2 - ny)
+    scaling_b = scaling_x * scaling_y
+    
+    scaling = ifelse(outside_frame, 0, 
+              ifelse(only_smooth_x, scaling_x,
+              ifelse(only_smooth_y, scaling_y, 
+              ifelse(smooth_both, scaling_b, 1))))
+
+    @inbounds d̂[i, j, k] = scaling * d̂[i, j, k]
+end
