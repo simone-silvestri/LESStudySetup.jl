c6daec25a84403c09a4e1c29c45eeb25ad6d1bff
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-07-16 13:30:53 -0400

    more experiments

--- experiments/experiments.jl
+++ experiments/experiments.jl
@@ -2,4 +2,6 @@ include("hydrostatic_experiment.jl")
 
 using Statistics: mean
 
-run_experiment!("free_surface_short_test_100_wind_00"; Q = 50.0, τw = 0.1, Δh = 100)
+run_experiment!("free_surface_short_test_050_wind_01"; Q = 50.0, τw = 0.1, Δh = 100)
+run_experiment!("free_surface_short_test_050_wind_02"; Q = 50.0, τw = 0.2, Δh = 100)
+run_experiment!("free_surface_short_test_000_wind_00"; Q = 00.0, τw = 0.0, Δh = 100)
