83e3aa0f06a480058256a9e59f5a634310aced9a
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2024-07-16 13:31:42 -0400

    adding a higher dT experiment

--- experiments/experiments.jl
+++ experiments/experiments.jl
@@ -2,6 +2,7 @@ include("hydrostatic_experiment.jl")
 
 using Statistics: mean
 
-run_experiment!("free_surface_short_test_050_wind_01"; Q = 50.0, τw = 0.1, Δh = 100)
-run_experiment!("free_surface_short_test_050_wind_02"; Q = 50.0, τw = 0.2, Δh = 100)
-run_experiment!("free_surface_short_test_000_wind_00"; Q = 00.0, τw = 0.0, Δh = 100)
+run_experiment!("free_surface_short_test_050_wind_01_dTf_2"; Q = 50.0, τw = 0.1, Δh = 100)
+run_experiment!("free_surface_short_test_050_wind_02_dTf_2"; Q = 50.0, τw = 0.2, Δh = 100)
+run_experiment!("free_surface_short_test_000_wind_00_dTf_2"; Q = 00.0, τw = 0.0, Δh = 100)
+run_experiment!("free_surface_short_test_000_wind_00_dTf_5"; Q = 00.0, τw = 0.0, Δh = 100, ΔTᶠ = 5.0)
